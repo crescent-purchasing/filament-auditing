4f90f95566f6baeb858662c74ad864c7d22fcbb0
aSeriousDeveloper <39706150+aSeriousDeveloper@users.noreply.github.com>
2025-05-01 14:23:20 +0000

    Fix styling

--- src/FilamentAuditingServiceProvider.php
+++ src/FilamentAuditingServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace CrescentPurchasing\FilamentAuditing;
 
+use CrescentPurchasing\FilamentAuditing\Commands\FilamentAuditingCommand;
+use CrescentPurchasing\FilamentAuditing\Testing\TestsFilamentAuditing;
 use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Asset;
 use Filament\Support\Assets\Css;
@@ -13,8 +15,6 @@ use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use CrescentPurchasing\FilamentAuditing\Commands\FilamentAuditingCommand;
-use CrescentPurchasing\FilamentAuditing\Testing\TestsFilamentAuditing;
 
 class FilamentAuditingServiceProvider extends PackageServiceProvider
 {
