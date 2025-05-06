97bbe7861ff3e7e50cab7b2b245908c5ebbb8fb7
Robert Lyons <r.lyons@thecpc.ac.uk>
2025-05-06 16:43:38 +0100

    make audit properties more consistent & add type property

--- src/Audit.php
+++ src/Audit.php
@@ -2,37 +2,54 @@
 
 namespace CrescentPurchasing\FilamentAuditing;
 
+use CrescentPurchasing\FilamentAuditing\Contracts\AuditContract;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 use OwenIt\Auditing\Models\Audit as BaseAudit;
 
-class Audit extends BaseAudit
+
+/**
+ * @property-read string $title
+ * @property-read string $name
+ * @property-read string $type
+ */
+class Audit extends BaseAudit implements AuditContract
 {
     protected function title(): Attribute
     {
-        $getName = function (mixed $value, array $attributes): string {
+        $getTitle = function (mixed $value, array $attributes): string {
             return __('filament-auditing::audit.title', [
-                'type' => self::formatAuditableType($attributes['auditable_type']),
+                'type' => $this->type,
                 'id' => $attributes['auditable_id'],
                 'timestamp' => $attributes['created_at'],
             ]);
         };
 
-        return Attribute::make(get: $getName);
+        return Attribute::make(get: $getTitle);
     }
 
     protected function name(): Attribute
     {
         $getName = function (mixed $value, array $attributes): string {
             return __('filament-auditing::audit.name', [
-                'type' => self::formatAuditableType($attributes['auditable_type']),
+                'type' => $this->type,
                 'id' => $attributes['auditable_id'],
             ]);
         };
 
         return Attribute::make(get: $getName);
+    }
+
+    protected function type(): Attribute
+    {
+        $getType = function (mixed $value, array $attributes): string {
+            return Str::of($attributes['auditable_type'])->classBasename()->headline()->toString();
+        };
+
+        return Attribute::make(get: $getType);
     }
 
     /**
