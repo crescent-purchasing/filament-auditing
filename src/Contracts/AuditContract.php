d89def911def8c83278a19decacf799f32b14dcc
aSeriousDeveloper <39706150+aSeriousDeveloper@users.noreply.github.com>
2025-05-07 14:08:55 +0000

    Fix styling

--- src/Contracts/AuditContract.php
+++ src/Contracts/AuditContract.php
@@ -3,18 +3,13 @@
 namespace CrescentPurchasing\FilamentAuditing\Contracts;
 
 use CrescentPurchasing\FilamentAuditing\Audit;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Foundation\Auth\User;
 
 /**
  * @phpstan-require-extends Audit
  */
 interface AuditContract
 {
-    /**
-     * @return BelongsTo
-     */
     public function owner(): BelongsTo;
 
     public function getModifiedByType(string $type = 'new'): array;
