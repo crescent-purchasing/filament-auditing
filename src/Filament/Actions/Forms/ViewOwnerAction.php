641db15617b026b2bc86792ea481a882ac332410
Robert Lyons <r.lyons@thecpc.ac.uk>
2025-05-08 15:32:12 +0100

    ViewOwnerAction for Forms, Tables, and Pages refactor name from User to Owner

---
+++ src/Filament/Actions/Forms/ViewOwnerAction.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace CrescentPurchasing\FilamentAuditing\Filament\Actions\Forms;
+
+use CrescentPurchasing\FilamentAuditing\Filament\Actions\Concerns\ViewsOwners;
+use CrescentPurchasing\FilamentAuditing\FilamentAuditingPlugin;
+use Filament\Forms\Components\Actions\Action;
+use Filament\Resources\Resource as FilamentResource;
+use Illuminate\Database\Eloquent\Model;
+
+class ViewOwnerAction extends Action
+{
+    use ViewsOwners;
+
+    protected function setUp(): void
+    {
+        $this->iconButton();
+
+        $this->label(function (Model $record): string {
+            $owner = $this->getOwner($record);
+
+            /** @var class-string<FilamentResource> $ownerResource */
+            $ownerResource = filament()->getModelResource($owner);
+
+            return __('filament-auditing::resource.actions.view.title', [
+                'title' => $ownerResource::getRecordTitle($record),
+            ]);
+        });
+    }
+}
---
+++ src/Filament/Actions/ViewOwnerAction.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace CrescentPurchasing\FilamentAuditing\Filament\Actions;
+
+use CrescentPurchasing\FilamentAuditing\Filament\Actions\Concerns\ViewsOwners;
+use Filament\Actions\Action;
+
+class ViewOwnerAction extends Action
+{
+    use ViewsOwners;
+
+}
