8613050aada9609a90d5fcbe3187bd9f99d5acc6
Robert Lyons <r.lyons@thecpc.ac.uk>
2025-05-09 12:44:59 +0100

    refactor lang

--- resources/lang/en/resource.php
+++ resources/lang/en/resource.php
@@ -2,6 +2,7 @@
 
 return [
     'title' => 'Audit History',
+    'relation_title' => 'Audit History',
     'record_title' => 'Snapshot - :record (:id) at :timestamp',
     'actions' => [
         'restore_audit' => [
@@ -16,13 +17,29 @@ return [
             'title' => 'View :title',
         ],
     ],
-    'relation' => [
-        'title' => 'Audit History',
-    ],
-    'table' => [
-        'columns' => [
-            'created_at' => 'Recorded at',
-            'created_at_since' => 'Recorded from',
+    'fields' => [
+        'auditable_type' => 'Record Type',
+        'audited_fields' => 'Audited fields',
+        'created_at' => 'Recorded at',
+        'created_at_since' => 'Recorded from',
+        'event' => 'Event',
+        'ip_address' => 'IP address',
+        'query' => 'Advanced',
+        'tags' => 'Tags',
+        'url' => 'URL',
+        'user_agent' => 'User agent',
+        'user' => [
+            'label' => 'Owner',
+            'id' => 'ID',
+            'email' => 'Email',
         ],
+        'id' => 'id',
+    ],
+    'tabs' => [
+        'label' => 'Audit data',
+        'meta' => 'Meta data',
+        'new' => 'New (After)',
+        'old' => 'Old (Before)',
+        'user' => 'User data',
     ],
 ];
