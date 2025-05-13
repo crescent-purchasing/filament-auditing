bf01ea795637dd1846a0dd18bf5978e2f5ea261e
Robert Lyons <r.lyons@thecpc.ac.uk>
2025-05-13 14:28:42 +0100

    HasUsers trait for specifying polymorphic user types

---
+++ src/Concerns/HasUsers.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace CrescentPurchasing\FilamentAuditing\Concerns;
+
+use Closure;
+use Filament\Resources\Resource as FilamentResource;
+use Illuminate\Foundation\Auth\User;
+
+trait HasUsers
+{
+    /** @var class-string<User>[]|Closure */
+    protected array | Closure $users = [];
+
+    /**
+     * @return class-string<User>[]|null
+     */
+    public function getUsers(): ?string
+    {
+        return $this->evaluate($this->users);
+    }
+
+    /**
+     * @param  class-string<User>[]|Closure  $users
+     * @return $this
+     */
+    public function users(array | Closure $users): static
+    {
+        $this->users = $users;
+
+        return $this;
+    }
+}
