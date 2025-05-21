36e032aa42e3be5d530b67854c3b33c47b3d71eb
Robert Lyons <r.lyons@thecpc.ac.uk>
2025-05-21 14:35:16 +0100

    update default test case and move pregenerated tests to /tests/src

--- tests/Pest.php
+++ tests/Pest.php
@@ -2,4 +2,4 @@
 
 use CrescentPurchasing\FilamentAuditing\Tests\TestCase;
 
-uses(TestCase::class)->in(__DIR__);
+uses(TestCase::class)->in(__DIR__ . '/src');
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,6 +5,8 @@ namespace CrescentPurchasing\FilamentAuditing\Tests;
 use BladeUI\Heroicons\BladeHeroiconsServiceProvider;
 use BladeUI\Icons\BladeIconsServiceProvider;
 use CrescentPurchasing\FilamentAuditing\FilamentAuditingServiceProvider;
+use CrescentPurchasing\FilamentAuditing\Tests\Filament\TestingPanelProvider;
+use CrescentPurchasing\FilamentAuditing\Tests\Models\User;
 use Filament\Actions\ActionsServiceProvider;
 use Filament\FilamentServiceProvider;
 use Filament\Forms\FormsServiceProvider;
@@ -13,23 +15,71 @@ use Filament\Notifications\NotificationsServiceProvider;
 use Filament\Support\SupportServiceProvider;
 use Filament\Tables\TablesServiceProvider;
 use Filament\Widgets\WidgetsServiceProvider;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
+use OwenIt\Auditing\AuditingServiceProvider;
+use OwenIt\Auditing\Resolvers\IpAddressResolver;
+use OwenIt\Auditing\Resolvers\UrlResolver;
+use OwenIt\Auditing\Resolvers\UserAgentResolver;
+use OwenIt\Auditing\Resolvers\UserResolver;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
 
 class TestCase extends Orchestra
 {
+    public function getEnvironmentSetUp($app): void
+    {
+        config()->set('database.default', 'testing');
+        config()->set('database.connections.testing', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+
+        // Audit
+        config()->set('audit.drivers.database.table', 'audit_testing');
+        config()->set('audit.drivers.database.connection', 'testing');
+        config()->set('audit.user.morph_prefix', 'prefix');
+        config()->set('audit.user.resolver', UserResolver::class);
+        config()->set('audit.user.guards', [
+            'web',
+            'api',
+        ]);
+        config()->set('auth.guards.api', [
+            'driver' => 'session',
+            'provider' => 'users',
+        ]);
+
+        config()->set('audit.resolvers.url', UrlResolver::class);
+        config()->set('audit.resolvers.ip_address', IpAddressResolver::class);
+        config()->set('audit.resolvers.user_agent', UserAgentResolver::class);
+
+        config()->set('audit.console', true);
+        config()->set('audit.empty_values', true);
+        config()->set('audit.queue.enable', true);
+    }
+
     protected function setUp(): void
     {
         parent::setUp();
 
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'CrescentPurchasing\\FilamentAuditing\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
-        );
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+
+        $console = config()->get('audit.console', true);
+        config()->set('audit.console', false);
+
+        $user = User::factory()->create([
+            'is_admin' => true,
+            'first_name' => 'Michael',
+            'last_name' => 'Morbius',
+            'email' => 'dr.morbius@example.com',
+        ]);
+
+        test()->admin = $user;
+
+        config()->set('audit.console', $console);
     }
 
-    protected function getPackageProviders($app)
+    protected function getPackageProviders($app): array
     {
         return [
             ActionsServiceProvider::class,
@@ -44,17 +94,9 @@ class TestCase extends Orchestra
             SupportServiceProvider::class,
             TablesServiceProvider::class,
             WidgetsServiceProvider::class,
+            AuditingServiceProvider::class,
             FilamentAuditingServiceProvider::class,
+            TestingPanelProvider::class,
         ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_filament-auditing_table.php.stub';
-        $migration->up();
-        */
     }
 }
