cee9ab9598bcdea6f07c5b4f7ba3a7f88f868ef9
Robert Lyons <r.lyons@thecpc.ac.uk>
2025-05-22 13:10:28 +0100

    tests for Filament/RelationManagers/AuditsRelationManager

---
+++ tests/src/Filament/RelationManagers/AuditsRelationManagerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use CrescentPurchasing\FilamentAuditing\Filament\RelationManagers\AuditsRelationManager;
+use CrescentPurchasing\FilamentAuditing\Tests\Filament\Resources\Article\Pages\EditArticle;
+use CrescentPurchasing\FilamentAuditing\Tests\Models\Article;
+use function Pest\Livewire\livewire;
+
+it('can render relation manager', function () {
+    test()->actingAs(test()->admin);
+
+    $article = Article::factory()->create();
+
+    livewire(AuditsRelationManager::class, [
+        'ownerRecord' => $article,
+        'pageClass' => EditArticle::class,
+    ])->assertSuccessful();
+});
+
+it('can list related audits', function () {
+    test()->actingAs(test()->admin);
+
+    $article = Article::factory()->create([
+        'title' => ' I have been created!',
+    ]);
+
+    $article->update([
+        'title' => 'I have been updated!',
+    ]);
+
+    livewire(AuditsRelationManager::class, [
+        'ownerRecord' => $article,
+        'pageClass' => EditArticle::class,
+    ])->assertCanSeeTableRecords($article->audits);
+});
