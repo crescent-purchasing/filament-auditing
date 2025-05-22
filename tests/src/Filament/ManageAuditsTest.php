64d86523df6fc2c95ddf9efbcd416f81eada0040
Robert Lyons <r.lyons@thecpc.ac.uk>
2025-05-22 13:10:39 +0100

    test for viewing a specific Audit

--- tests/src/Filament/ManageAuditsTest.php
+++ tests/src/Filament/ManageAuditsTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Carbon\Carbon;
+use CrescentPurchasing\FilamentAuditing\Filament\Actions\Tables\ViewAuditAction;
 use CrescentPurchasing\FilamentAuditing\Filament\ManageAudits;
 use CrescentPurchasing\FilamentAuditing\Tests\Models\Article;
 use OwenIt\Auditing\Models\Audit;
@@ -8,22 +9,41 @@ use OwenIt\Auditing\Models\Audit;
 use function Pest\Livewire\livewire;
 
 it('can view Audit History', function () {
-    $now = Carbon::now();
-
     test()->actingAs(test()->admin);
 
-    Article::factory()->create([
-        'title' => 'How To Audit Eloquent Models',
-        'content' => 'First step: install the laravel-auditing package.',
-        'user_id' => test()->admin,
-        'reviewed' => 1,
-        'published_at' => $now,
-    ]);
+    Article::factory()->create();
 
     $audits = Audit::all();
 
-    expect($audits)->toHaveCount(1);
-
     livewire(ManageAudits::class)
         ->assertCanSeeTableRecords($audits);
+});
+
+it('can view a specific Audit', function () {
+    test()->actingAs(test()->admin);
+
+    $article = Article::factory()->create([
+        'title' => 'I have been created!',
+    ]);
+
+    $article->update([
+        'title' => 'I have been updated!',
+    ]);
+
+    $audit = Audit::latest('id')->firstOrFail();
+
+    livewire(ManageAudits::class)
+        ->assertCountTableRecords(2)
+        ->mountTableAction(ViewAuditAction::class, $audit)
+        ->assertTableActionDataSet([
+            'created_at' => $audit->created_at,
+            'event' => $audit->event,
+            'user.email' => test()->admin->email,
+            'old_values.title' => 'I have been created!',
+            'new_values.title' => 'I have been updated!',
+        ])
+        ->assertSee([
+            'I have been created!',
+            'I have been updated!',
+        ]);
 });
