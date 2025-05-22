b2e789750ed1daf9eafe47b910923f04f36f9531
Robert Lyons <r.lyons@thecpc.ac.uk>
2025-05-22 16:28:27 +0100

    test for Filament/Actions/**/ViewAuditableAction

---
+++ tests/src/Filament/Actions/ViewAuditableActionTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use CrescentPurchasing\FilamentAuditing\Filament\Actions\Tables\ViewAuditableAction;
+use CrescentPurchasing\FilamentAuditing\Filament\ManageAudits;
+use CrescentPurchasing\FilamentAuditing\Tests\Filament\Resources\Article\ArticleResource;
+use CrescentPurchasing\FilamentAuditing\Tests\Models\Article;
+use OwenIt\Auditing\Models\Audit;
+use function Pest\Livewire\livewire;
+
+it('Can see the url of the audit record', function () {
+    test()->actingAs(test()->admin);
+
+    $article = Article::factory()->create();
+
+    $audit = Audit::latest('id')->firstOrFail();
+
+    $articleUrl = ArticleResource::getGlobalSearchResultUrl($article);
+
+    livewire(ManageAudits::class)
+        ->assertTableActionHasUrl(ViewAuditableAction::class, $articleUrl, $audit);
+});
