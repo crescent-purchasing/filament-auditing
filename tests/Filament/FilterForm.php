764b2d1d4276c1b33f6033e782b9d8159a319e0d
Robert Lyons <r.lyons@thecpc.ac.uk>
2025-05-28 12:35:28 +0100

    livewire class for displaying Filter Form workaround because query builder form testing is a nightmare

---
+++ tests/Filament/FilterForm.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace CrescentPurchasing\FilamentAuditing\Tests\Filament;
+
+use CrescentPurchasing\FilamentAuditing\Filament\Filters\QueryBuilder\AuditUserConstraint;
+use CrescentPurchasing\FilamentAuditing\Filament\Filters\QueryBuilder\AuditUserOperator;
+use CrescentPurchasing\FilamentAuditing\FilamentAuditingPlugin;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Form;
+use Livewire\Component;
+
+/**
+ * @property Form $form
+ */
+class FilterForm extends Component implements HasForms
+{
+    use InteractsWithForms;
+
+    public ?array $data = [];
+
+    public function mount(): void
+    {
+        $this->form->fill();
+    }
+
+    public function form(Form $form): Form
+    {
+        $constraint = AuditUserConstraint::make('user')
+            ->selectable(
+                AuditUserOperator::make()
+                    ->titleAttribute('email')
+                    ->types(FilamentAuditingPlugin::get()->getUsers())
+            );
+
+        /** @var AuditUserOperator $operator */
+        $operator = $constraint->getOperator('isRelatedTo');
+
+        $operator->constraint($constraint);
+
+        return $form
+            ->schema($operator->getFormSchema())
+            ->statePath('data');
+    }
+
+    public function render()
+    {
+        return <<<'HTML'
+        <div>
+            {{ $this->form }}
+        </div>
+        HTML;
+    }
+}
